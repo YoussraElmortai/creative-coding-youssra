fdb6f3a8355b36a0c7259bba6749eb57166ff51b
youssra <youssramortai01@gmail.com>
2023-12-05 12:18:29 +0100

    installing gsap :four_leaf_clover:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script>
 
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { TextPlugin } from "gsap/TextPlugin";
+
+
+gsap.registerPlugin(ScrollTrigger,TextPlugin);
+
 </script>
 
 <h1>DAYUM</h1>
