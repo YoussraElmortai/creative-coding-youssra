24736b7e7955e28f25bf7281de1e8441ad0dcd51
youssra <youssramortai01@gmail.com>
2023-12-06 14:35:41 +0100

    global css toegevoegd :four_leaf_clover:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
-import { onMount } from 'svelte';
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { TextPlugin } from "gsap/TextPlugin";
+// import { gsap } from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// import { TextPlugin } from 'gsap/TextPlugin';
 
-
-gsap.registerPlugin(ScrollTrigger,TextPlugin);
-
+// gsap.registerPlugin(ScrollTrigger, TextPlugin);
 </script>
 
 <h1>DAYUM</h1>
+
+
+<style>
+    
+</style>
