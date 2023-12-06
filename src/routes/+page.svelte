e165a28e83172ecf5236f27759bcd8c15ab15561
youssra <youssramortai01@gmail.com>
2023-12-06 21:32:09 +0100

    onmount uitproberen :four_leaf_clover:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,24 @@
 
 	gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
+	 // Function to animate the blob element
+  function animateBlob() {
+    gsap.to('.blob', {
+      duration: 1,
+      scale: 1.2, // Scale to 1.2 times its original size
+      ease: 'power2.out' // Easing function
+    });
+  }
+
+  onMount(() => {
+    animateBlob(); // Trigger the animation when the component mounts
+  });
 	
 </script>
 
 <header><img src="/logo.png" alt="plantswap"></header>
 <section class="banner"></section>
+<div class="blob"></div>
 
 <style>
 	header{
@@ -30,5 +43,12 @@
 		background-color: var(--card-background);
 		border-bottom: solid 3px var(--card-background);
 	}
+
+	 .blob {
+    /* Add styles for your blob element */
+    width: 100px;
+    height: 100px;
+    background-color: red; /* Replace with your desired blob styles */
+  }
 
 </style>
